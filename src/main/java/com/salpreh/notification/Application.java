7b364a9b8297258d2d3b24d1660793c067bd5577
salpreh <salva.perez46@gmail.com>
2022-05-11 11:49:17 +0200

    App class rename

--- src/main/java/com/salpreh/notification/Application.java
+++ src/main/java/com/salpreh/notification/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirebebaseSrvApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirebebaseSrvApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
