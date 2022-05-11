0019553297ac8ec0b8407f4e13892e67697f1b54
salpreh <salva.perez46@gmail.com>
2022-05-11 11:35:38 +0200

    Init repository

---
+++ src/main/java/com/salpreh/notification/FirebebaseSrvApplication.java
@@ -0,0 +1,13 @@
+package com.salpreh.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebebaseSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebebaseSrvApplication.class, args);
+	}
+
+}
